d5c2b37d25b222a89fe13e103a3f22a29ad6357d
greepar <greepar@outlook.com>
2026-08-28 05:39:49 +0800

    Test built-in shared contracts in single-file host

---
+++ ShiroBot.SharedContractPluginProbe/SharedContractPluginProbe.cs
@@ -0,0 +1,16 @@
+using ShiroBot.SDK.Core;
+using ShiroBot.SDK.Plugin;
+
+[assembly: ShiroBotApiCompatibility("0.8", "0.8")]
+
+namespace ShiroBot.SharedContractPluginProbe;
+
+[BotPlugin(
+    "SharedContractPluginProbe",
+    Name = "Shared contract plugin probe",
+    Version = "0.8.0",
+    SharedAssemblies = "ShiroBot.Model.QQ")]
+public sealed class SharedContractPluginProbe : PluginBase
+{
+    public override string Name => "SharedContractPluginProbe";
+}
